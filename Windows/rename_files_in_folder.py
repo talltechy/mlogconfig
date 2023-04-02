1ba5b36f09eafd9bf9e98455a11073fdc2978feb
Matt <matt@mattwyen.me>
2023-04-02 12:55:59 -0400

    document and update

--- Windows/rename_files_in_folder.py
+++ Windows/rename_files_in_folder.py
@@ -1,3 +1,13 @@
+# This code renames all files ending with .pgsql to .sql, except for those in the exclude_list
+# This code is used as a part of a larger program that is used to create a database from sql files
+# The code is executed by the user, and the result is the renaming of the files in the startdir
+# The user is prompted for the startdir and exclude_list, and the code is executed on the files in the startdir
+# The user is prompted for the startdir and exclude_list, and the code is executed on the files in the startdir
+# The code is executed in the following steps:
+# 1. The user is prompted for the startdir and exclude_list
+# 2. The code iterates through all files in the startdir, and all subdirectories, except those in the exclude_list
+# 3. If the file ends with .pgsql, the file is renamed to end with .sql
+
 #!/usr/bin/env python3
 import os
 import dir_exclude
@@ -12,6 +22,7 @@ try:
                 newname = infilename.replace('.pgsql', '.sql')
                 try:
                     output = os.rename(infilename, newname)
+                    print(f"File renamed from {infilename} to {newname}")
                 except Exception as e:
                     print(f"Error occurred: {e}") # Handle any exception that occurs during file renaming
 except Exception as e:
