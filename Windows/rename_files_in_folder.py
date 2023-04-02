bb4a99adf2efc6838c1163085829e9d074c8fb13
Matt <matt@mattwyen.me>
2023-04-02 12:56:51 -0400

    clean up code

--- Windows/rename_files_in_folder.py
+++ Windows/rename_files_in_folder.py
@@ -13,17 +13,14 @@ import os
 import dir_exclude
 import dir_startdir
 
-try:
-    for root, dirs, files in os.walk(dir_startdir.startdir):
-        dirs[:] = [d for d in dirs if d not in dir_exclude.exclude_list]
-        for filename in files:
-            if filename.endswith('.pgsql'): # Check if file name ends with .pgsql
-                infilename = os.path.join(root,filename)
-                newname = infilename.replace('.pgsql', '.sql')
-                try:
-                    output = os.rename(infilename, newname)
-                    print(f"File renamed from {infilename} to {newname}")
-                except Exception as e:
-                    print(f"Error occurred: {e}") # Handle any exception that occurs during file renaming
-except Exception as e:
-    print(f"Error occurred: {e}") # Handle any exception that occurs during os.walk
+for root, dirs, files in os.walk(dir_startdir.startdir):
+    dirs[:] = [d for d in dirs if d not in dir_exclude.exclude_list]
+    for filename in files:
+        if filename.endswith('.pgsql'):
+            infilename = os.path.join(root,filename)
+            newname = infilename.replace('.pgsql', '.sql')
+            try:
+                output = os.rename(infilename, newname)
+                print(f"File renamed from {infilename} to {newname}")
+            except Exception as e:
+                print(f"Error occurred: {e}")
