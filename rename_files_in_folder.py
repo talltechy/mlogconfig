6eddab68f4e98ea72ae26b80ae01ca2312808fae
Matt <matt@mattwyen.me>
2023-04-02 15:58:25 -0400

    fix logging

--- rename_files_in_folder.py
+++ rename_files_in_folder.py
@@ -1,64 +1,17 @@
 import logging
 import logging.handlers
 import os
-import platform
 import re
-import subprocess
-import sys
+from logger import setup_logging
 
-
-def setup_logging():\n    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.INFO)
-
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-    file_handler = logging.FileHandler('file_renamer.log')
-    file_handler.setFormatter(formatter)
-    root_logger.addHandler(file_handler)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    root_logger.addHandler(console_handler)
-
-    if platform.system() == 'Linux':
-        try:
-            syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
-            syslog_handler.setFormatter(formatter)
-            root_logger.addHandler(syslog_handler)
-        except FileNotFoundError:
-            pass
-    elif platform.system() == 'Darwin':
-        try:
-            syslog_handler = logging.handlers.SysLogHandler(address='/var/run/syslog')
-            syslog_handler.setFormatter(formatter)
-            root_logger.addHandler(syslog_handler)
-        except FileNotFoundError:
-            pass
-    elif platform.system() == 'Windows':
-        try:
-            nt_event_log_handler = logging.handlers.NTEventLogHandler("FileRenamer")
-            nt_event_log_handler.setFormatter(formatter)
-            root_logger.addHandler(nt_event_log_handler)
-        except ImportError:
-            print("win32api not found, please install it using: pip install pywin32", file=sys.stderr)
-            user_input = input("Do you want to install the required package now? (yes/no): ").lower()
-            while user_input not in ['yes', 'no']:
-                user_input = input("Invalid input! Do you want to install the required package now? (yes/no): ").lower()
-            if user_input == 'yes':
-                subprocess.run([sys.executable, "-m", "pip", "install", "pywin32"])
-            else:
-                print("Please install the required package manually and run the script again.")
-                sys.exit(1)
-
+log_file_path = 'file_renamer.log'
 
 def is_valid_directory(directory):
     return os.path.isdir(directory)
 
-
 def is_valid_extension(extension):
-    extension_pattern = r'^\.\w+$'
+    extension_pattern = r'^\.[\w]+$'
     return re.match(extension_pattern, extension)
-
 
 def get_user_input():
     while True:
@@ -93,7 +46,6 @@ def get_user_input():
 
     return startdir, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions
 
-
 def rename_file(root, filename, old_extension, new_extension):
     infilename = os.path.join(root, filename)
     newname = os.path.join(root, filename.replace(old_extension, new_extension))
@@ -107,7 +59,6 @@ def rename_file(root, filename, old_extension, new_extension):
         message = f"Error occurred: {e}"
         print(message)
         logging.error(message)
-
 
 def rename_files(startdir, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions, depth=0):
     default_exclude = [
@@ -127,21 +78,26 @@ def rename_files(startdir, old_extension, new_extension, ignore_default_exclusio
         logging.warning(message)
         return
 
-    for root, dirs, files in os.walk(startdir):
-        dirs[:] = [d for d in dirs if d not in exclude]
-        for filename in files:
-            if filename.endswith(old_extension):
-                rename_file(root, filename, old_extension, new_extension)
+    try:
+        for root, dirs, files in os.walk(startdir):
+            dirs[:] = [d for d in dirs if d not in exclude]
+            for filename in files:
+                if filename.endswith(old_extension):
+                    rename_file(root, filename, old_extension, new_extension)
 
-        for subdir in dirs:
-            subdir_path = os.path.join(root, subdir)
-            rename_files(subdir_path, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions, depth=depth+1)
+            for subdir in dirs:
+                subdir_path = os.path.join(root, subdir)
+                rename_files(subdir_path, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions, depth=depth+1)
+    except FileNotFoundError as e:
+        message = f"Error occurred: {e}"
+        print(message)
+        logging.error(message)
 
 
 def main():
     global MAX_DEPTH
     MAX_DEPTH = int(os.environ.get('MAX_DEPTH', 5))
-    setup_logging()
+    setup_logging(log_file_path)
     startdir, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions = get_user_input()
     logging.info(f"Starting directory: {startdir}")
     logging.info(f"Old file extension: {old_extension}")
