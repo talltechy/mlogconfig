27e72a26c7b7edc1167bd701ff811715df1b412e
Matt <matt@mattwyen.me>
2023-04-02 15:53:19 -0400

    fix incorrect file name

--- rename_files_in_folder.py
+++ rename_files_in_folder.py
@@ -7,11 +7,7 @@ import subprocess
 import sys
 
 
-MAX_DEPTH = 5
-
-
-def setup_logging():
-    root_logger = logging.getLogger()
+def setup_logging():\n    root_logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)
 
     formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -19,6 +15,10 @@ def setup_logging():
     file_handler = logging.FileHandler('file_renamer.log')
     file_handler.setFormatter(formatter)
     root_logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    root_logger.addHandler(console_handler)
 
     if platform.system() == 'Linux':
         try:
@@ -43,8 +43,7 @@ def setup_logging():
             print("win32api not found, please install it using: pip install pywin32", file=sys.stderr)
             user_input = input("Do you want to install the required package now? (yes/no): ").lower()
             while user_input not in ['yes', 'no']:
-                user_input = input("Do you want to install the required package now? (yes/no): ").lower()
-
+                user_input = input("Invalid input! Do you want to install the required package now? (yes/no): ").lower()
             if user_input == 'yes':
                 subprocess.run([sys.executable, "-m", "pip", "install", "pywin32"])
             else:
@@ -52,62 +51,40 @@ def setup_logging():
                 sys.exit(1)
 
 
-def sanitize_input(input_string):
-    return input_string.strip()
-
-
 def is_valid_directory(directory):
-    sanitized_dir = sanitize_input(directory)
-    if not os.path.isdir(sanitized_dir):
-        print("Invalid directory. Please enter a valid directory.")
-        return False, sanitized_dir
-    return True, sanitized_dir
+    return os.path.isdir(directory)
 
 
 def is_valid_extension(extension):
-    sanitized_ext = sanitize_input(extension)
     extension_pattern = r'^\.\w+$'
-    if not re.match(extension_pattern, sanitized_ext):
-        print("Invalid file extension. Please enter a valid file extension.")
-        return False, sanitized_ext
-    return True, sanitized_ext
-
-
-def add_dot_if_needed(extension):
-    if not extension.startswith('.'):
-        extension = '.' + extension
-    return extension
+    return re.match(extension_pattern, extension)
 
 
 def get_user_input():
     while True:
-        is_valid, startdir = is_valid_directory(input("Enter start directory: "))
-        if is_valid:
+        startdir = input("Enter start directory: ")
+        if is_valid_directory(startdir):
             break
+        print("Invalid directory. Please enter a valid directory.")
 
     while True:
-        is_valid, old_extension = is_valid_extension(add_dot_if_needed(input('Enter file extension to rename: ')))
-        if is_valid:
+        old_extension = input('Enter file extension to rename: ')
+        if is_valid_extension(old_extension):
             break
+        print("Invalid file extension. Please enter a valid file extension.")
 
     while True:
-        is_valid, new_extension = is_valid_extension(add_dot_if_needed(input("Enter new file extension: ")))
-        if is_valid:
+        new_extension = input("Enter new file extension: ")
+        if is_valid_extension(new_extension):
             break
+        print("Invalid file extension. Please enter a valid file extension.")
 
-    while True:
-        ignore_default_exclusions = input("Ignore default exclusions? (yes/no): ").lower()
-        if ignore_default_exclusions in ['yes', 'no']:
-            break
+    ignore_default_exclusions = input("Ignore default exclusions? (yes/no): ").lower() == 'yes'
 
     if not ignore_default_exclusions:
         custom_exclusions = input("Enter your own comma-separated exclusions, or leave blank for none: ").split(',')
         if custom_exclusions != ['']:
-            while True:
-                overwrite_existing_exclusions = input("Overwrite existing exclusions? (yes/no): ").lower()
-                if overwrite_existing_exclusions in ['yes', 'no']:
-                    break
-            overwrite_existing_exclusions = overwrite_existing_exclusions == 'yes'
+            overwrite_existing_exclusions = input("Overwrite existing exclusions? (yes/no): ").lower() == 'yes'
         else:
             overwrite_existing_exclusions = False
     else:
@@ -115,6 +92,21 @@ def get_user_input():
         overwrite_existing_exclusions = False
 
     return startdir, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions
+
+
+def rename_file(root, filename, old_extension, new_extension):
+    infilename = os.path.join(root, filename)
+    newname = os.path.join(root, filename.replace(old_extension, new_extension))
+
+    try:
+        os.rename(infilename, newname)
+        message = f"Renamed file: {infilename} to {newname}"
+        print(message)
+        logging.info(message)
+    except OSError as e:
+        message = f"Error occurred: {e}"
+        print(message)
+        logging.error(message)
 
 
 def rename_files(startdir, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions, depth=0):
@@ -139,18 +131,7 @@ def rename_files(startdir, old_extension, new_extension, ignore_default_exclusio
         dirs[:] = [d for d in dirs if d not in exclude]
         for filename in files:
             if filename.endswith(old_extension):
-                infilename = os.path.join(root, filename)
-                newname = os.path.join(root, filename.replace(old_extension, new_extension))
-
-                try:
-                    os.rename(infilename, newname)
-                    message = f"Renamed file: {infilename} to {newname}"
-                    print(message)
-                    logging.info(message)
-                except OSError as e:
-                    message = f"Error occurred: {e}"
-                    print(message)
-                    logging.error(message)
+                rename_file(root, filename, old_extension, new_extension)
 
         for subdir in dirs:
             subdir_path = os.path.join(root, subdir)
@@ -158,6 +139,8 @@ def rename_files(startdir, old_extension, new_extension, ignore_default_exclusio
 
 
 def main():
+    global MAX_DEPTH
+    MAX_DEPTH = int(os.environ.get('MAX_DEPTH', 5))
     setup_logging()
     startdir, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions = get_user_input()
     logging.info(f"Starting directory: {startdir}")
@@ -167,6 +150,7 @@ def main():
     logging.info(f"Custom exclusions: {custom_exclusions}")
     logging.info(f"Overwrite existing exclusions: {overwrite_existing_exclusions}")
     rename_files(startdir, old_extension, new_extension, ignore_default_exclusions, custom_exclusions, overwrite_existing_exclusions)
+
 
 if __name__ == '__main__':
     main()
