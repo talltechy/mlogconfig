6176d2be22efa7d404d387290f8e92cb048dbfce
Matt <matt@mattwyen.me>
2023-04-07 18:33:35 -0400

    optimize

--- mlogconfig-2.py
+++ mlogconfig-2.py
@@ -1,80 +1,96 @@
-"""
-mlogconfig.py
-A simple logging setup utility that configures logging with file,
-console, syslog, and Windows event log handlers.
-"""
-
 import os
 import platform
 import logging
+from logging import Formatter, StreamHandler, FileHandler
 import datetime
-
-from logging import Formatter, getLogger
-from logging.handlers import SysLogHandler
+import sys
+import pywintypes
 
 try:
     import win32evtlog
-    import win32event
     import win32security
+    import winerror
+    WIN32_AVAILABLE = True
 except ImportError:
-    win32evtlog = None
-    win32event = None
-    win32security = None
+    WIN32_AVAILABLE = False
+
+from logging.handlers import SysLogHandler
 
 
-from logging import FileHandler, StreamHandler
+class EventLogException(Exception):
+    """Custom exception for Windows event log errors."""
+    pass
+
 
 class WindowsEventLogHandler(logging.Handler):
     def __init__(self, appName):
-        logging.Handler.__init__(self)
+        """Initialize the Windows event log handler.
+
+        Args:
+            appName (str): The application name for the event log.
+        """
+        super().__init__()
         self.appName = appName
 
     def emit(self, record):
+        """Emit a log record to the Windows event log.
+
+        Args:
+            record (logging.LogRecord): The log record to emit.
+        """
+        if not WIN32_AVAILABLE:
+            return
+
         try:
             sid = win32security.LookupAccountName("", os.environ["USERNAME"])[0]
+            message = str(record.getMessage())
             win32evtlog.ReportEvent(
                 self.appName,
                 1,
-                strings=[str(record.getMessage())],
-                EventType=record.levelno,
-                EventCategory=0,
-                Sid=sid,
+                0,
+                record.levelno,
+                sid,
+                [message],
+                b"",
             )
-        except Exception as e:
+        except (EventLogException, pywintypes.error) as e:
+            if isinstance(e, pywintypes.error) and e.winerror == winerror.ERROR_FILE_NOT_FOUND:
+                raise FileNotFoundError(e.strerror) from None
+            else:
+                self.handleError(record)
+        except Exception:
             self.handleError(record)
-
 
 
 def validate_log_file(log_file_path, mode="a"):
     """
     Validate the log file path and create a file handler for it.
 
-    :param log_file_path: Path to the log file
-    :param mode: File mode for the log file, either 'a' (append), 'w' (overwrite), or 'n' (new file)
-    :return: File handler and the validated log file path
-    :rtype: tuple
+    Args:
+        log_file_path (str): Path to the log file.
+        mode (str, optional): File mode for the log file. Defaults to 'a'.
+    Returns:
+        tuple: File handler and the validated log file path.
     """
-    if mode not in ("a", "w", "n"):
-        raise ValueError(
-            "Invalid mode. Mode should be 'a' (append), 'w' (overwrite), or 'n' (new file)"
-        )
-
+    log_file_path = os.path.abspath(log_file_path)
     log_dir = os.path.dirname(log_file_path)
 
-    # Create directory if it doesn't exist
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
+    if not os.path.exists(log_file_path):
+        open(log_file_path, 'w').close()
+
+    if not os.path.isfile(log_file_path):
+        raise ValueError(f"'{log_file_path}' is not a valid file path.")
+
+    if mode not in ("a", "w", "x"):
+        raise ValueError(
+            "Invalid mode. Mode should be 'a' (append), 'w' (overwrite), or 'x' (new file)"
+        )
+
     if not os.access(log_dir, os.W_OK):
         raise PermissionError(f"The directory '{log_dir}' is not writeable.")
-
-    if mode == "n":
-        if os.path.exists(log_file_path):
-            raise FileExistsError(
-                f"The logfile '{log_file_path}' already exists. Please choose a different path for the new file."
-            )
-        else:
-            mode = "a"
 
     file_handler = FileHandler(log_file_path, mode=mode)
     return file_handler, log_file_path
@@ -86,21 +102,23 @@ def setup_logging(
     console_logging=False,
     syslog_logging=False,
     windows_event_logging=False,
+    log_level=logging.INFO,
 ):
     """
     Set up logging with a file and optionally a syslog or Windows event log handler.
 
-    :param log_file_path: Path to the log file
-    :param error_log_file_path: Path to the error log file
-    :param console_logging: Whether to enable console logging or not
-    :param syslog_logging: Whether to enable syslog logging or not
-    :param windows_event_logging: Whether to enable Windows event logging or not
-    :rtype: None
+    Args:
+        log_file_path (str): Path to the log file.
+        error_log_file_path (str): Path to the error log file.
+        console_logging (bool, optional): Whether to enable console logging or not. Defaults to False.
+        syslog_logging (bool, optional): Whether to enable syslog logging or not. Defaults to False.
+        windows_event_logging (bool, optional): Whether to enable Windows event logging or not. Defaults to False.
+        log_level (int, optional): Logging level. Defaults to logging.INFO.
     """
     file_handler, log_file_path = validate_log_file(log_file_path)
 
-    root_logger = getLogger()
-    root_logger.setLevel(logging.INFO)
+    root_logger = logging.getLogger()
+    root_logger.setLevel(log_level)
 
     format_str = "%(asctime)s - %(levelname)s: %(message)s"
     formatter = Formatter(format_str, datefmt="%Y-%m-%d %H:%M:%S")
@@ -109,6 +127,8 @@ def setup_logging(
     root_logger.addHandler(file_handler)
 
     error_file_handler, error_log_file_path = validate_log_file(error_log_file_path)
+    if os.path.abspath(log_file_path) == os.path.abspath(error_log_file_path):
+        raise ValueError("log_file_path and error_log_file_path should be different.")
     error_file_handler.setFormatter(formatter)
     error_file_handler.setLevel(logging.ERROR)
     root_logger.addHandler(error_file_handler)
@@ -128,30 +148,35 @@ def setup_logging(
         syslog_handler.setFormatter(formatter)
         root_logger.addHandler(syslog_handler)
 
-    if (
-        windows_event_logging
-        and platform.system() == "Windows"
-        and win32evtlog
-        and win32event
-    ):
+    if windows_event_logging:
         nt_event_log_handler = WindowsEventLogHandler(appName="mlogconfig")
         root_logger.addHandler(nt_event_log_handler)
 
 
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python mlogconfig.py <log_file_path> <error_log_file_path>")
+        sys.exit(1)
 
-if __name__ == "__main__":
+    log_file_path = sys.argv[1]
+    error_log_file_path = sys.argv[2]
+
     try:
         setup_logging(
-            "./log_file.log",
-            "./error_log_file.log",
+            log_file_path,
+            error_log_file_path,
             console_logging=True,
             syslog_logging=True,
             windows_event_logging=True,
+            log_level=logging.DEBUG,
         )
     except Exception as e:
-        error_log_file_path = "./error_log_file.log"
-        with open(error_log_file_path, "a") as error_log_file:
+        with open(error_log_file_path, "a", encoding="utf-8") as error_log_file:
             error_log_file.write(
                 f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - ERROR: {str(e)}\n"
             )
         raise
+
+
+if __name__ == "__main__":
+    main()
