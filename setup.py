bddbed370d4acafa56a33b63a2b203379a2abd5a
Matt <matt@mattwyen.me>
2023-04-06 23:18:48 -0400

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mlogconfig",
-    version="0.1.0",
+    version="0.1.1",
     author="Matt Wyen",
     author_email="matt@mattwyen.me",
     description="A simple logging setup utility that configures logging with file, console, syslog, and Windows event log handlers.",
